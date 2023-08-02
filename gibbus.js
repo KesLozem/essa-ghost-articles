6acb5f24c69aab0c9cfa1586c414b422ed8277f5
KesLozem <leskosem@gmail.com>
2023-08-02 12:25:44 +1000

    insert meta tags

--- gibbus.js
+++ gibbus.js
@@ -4,28 +4,56 @@ const { Client } =  require("@notionhq/client")
 const notion = new Client({ auth: process.env.NOTION_GIBBUS_KEY })
 const databaseId = process.env.NOTION_DB_ID
 
-async function addItem(text) {
-    try {
-      const response = await notion.pages.create({
-        parent: { database_id: databaseId },
-        properties: {
-          title: {
-            title:[
-              {
-                "text": {
-                  "content": text
-                }
+async function addPage(page) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: {
+          title:[
+            {
+              "text": {
+                "content": page.properties.title
               }
-            ]
+            }
+          ]
+        },
+        Author: {
+          rich_text: [
+            {
+              "text": {
+                "content": page.properties.author,
+                "link": null
+              }
+            }
+          ]
+        },
+        Date: {
+           "date": {
+            "start": page.properties.date
           }
         },
-      })
-      console.log(response)
-      console.log("Success! Entry added.")
-    } catch (error) {
-      console.error(error.body)
-    }
+        Tags: {
+          multi_select: page.properties.tags
+        }
+      },
+    })
+    console.log(response)
+    console.log("Success! Entry added.")
+  } catch (error) {
+    console.error(error.body)
   }
-  
-  addItem("<b>Yurts in Big Sur, California</b>")
-  
+}
+
+module.exports = addPage;
+
+// page = {
+//  properties: {
+//    title: "The \"Crate\" Depression",
+//    author: "Les Kosem",
+//    date: "2023-01-11",
+//    tags: [{"name": "Testing"}, {"name": "NotionAPI"}]
+//   }
+// }
+//  
+// addPage(page)
--- src.js
+++ src.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const GhostContentAPI = require('@tryghost/content-api');
+const addPage = require("./gibbus.js"); // addPage function
 
 const api = new GhostContentAPI({
     url: process.env.GHOST_SITE,
@@ -7,11 +8,20 @@ const api = new GhostContentAPI({
     version: "v5.0"
 });
 
-api.posts.browse({limit: 2, include: 'tags,authors'})
+api.posts.browse({filter: 'primary_author:les-kosem', filter: 'primary_author:les-kosem', filter: 'primary_author:les-kosem', limit: 1, include: 'tags,authors'})
 .then((posts) => {
     posts.forEach((post) => {
-        let json = {"title": post.title, "url": post.url, "date": post.created_at}
-        console.log(JSON.stringify(json))
+      let json = {"title": post.title, "url": post.url, "date": post.created_at, "content": post.html}
+      // console.log(JSON.stringify(json))
+      page = {
+        properties: {
+          title: post.title,
+          author: post.primary_author.name,
+          date: post.created_at,
+          tags: [{"name": "Testing"}, {"name": "NotionAPI"}]
+        }
+      } 
+      addPage(page);
     });
 })
 .catch((err) => {
